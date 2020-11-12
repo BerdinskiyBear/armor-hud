efbb8f758d3314479a5d669e8424bb8555d0deef
BerdinskiyBear <bad.system000@gmail.com>
2020-11-12 18:11:48 +0700

    Attack indicator at hotbar has its own separate offset and icons for empty slots in widget are shown.

--- src/main/java/ru/berdinskiybear/armorhud/mixin/InGameHudMixin.java
+++ src/main/java/ru/berdinskiybear/armorhud/mixin/InGameHudMixin.java
@@ -1,14 +1,18 @@
 package ru.berdinskiybear.armorhud.mixin;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
 import me.shedaniel.clothconfig2.gui.AbstractConfigScreen;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.options.AttackIndicator;
+import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Arm;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -24,9 +28,16 @@ import java.util.ArrayList;
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin extends DrawableHelper {
 
+    private static final Identifier EMPTY_HELMET_SLOT_TEXTURE = new Identifier("item/empty_armor_slot_helmet");
+    private static final Identifier EMPTY_CHESTPLATE_SLOT_TEXTURE = new Identifier("item/empty_armor_slot_chestplate");
+    private static final Identifier EMPTY_LEGGINGS_SLOT_TEXTURE = new Identifier("item/empty_armor_slot_leggings");
+    private static final Identifier EMPTY_BOOTS_SLOT_TEXTURE = new Identifier("item/empty_armor_slot_boots");
+
+    private static final Identifier BLOCK_ATLAS_TEXTURE = new Identifier("textures/atlas/blocks.png");
+
     private int armorWidgetX;
     private int armorWidgetY;
-    ArmorHudConfig currentConfig;
+    private ArmorHudConfig currentArmorHudConfig;
 
     @Shadow
     protected abstract PlayerEntity getCameraPlayer();
@@ -47,23 +58,24 @@ public abstract class InGameHudMixin extends DrawableHelper {
     @Inject(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;setZOffset(I)V", ordinal = 1))
     public void renderArmorWidget(float tickDelta, MatrixStack matrices, CallbackInfo ci) {
         PlayerEntity playerEntity = this.getCameraPlayer();
-        currentConfig = this.client.currentScreen instanceof AbstractConfigScreen && this.client.currentScreen.getTitle() == ArmorHudConfigScreenBuilder.title ? ArmorHudConfigScreenBuilder.previewConfig : ArmorHudMod.getCurrentConfig();
+        currentArmorHudConfig = this.client.currentScreen instanceof AbstractConfigScreen && this.client.currentScreen.getTitle() == ArmorHudConfigScreenBuilder.title ? ArmorHudConfigScreenBuilder.previewConfig : ArmorHudMod.getCurrentConfig();
         int step = 20;
         int width = 22;
         int height = 22;
         int center = this.scaledWidth / 2;
         int defaultHotbarOffset = 98;
         int defaultOffhandSlotOffset = 29;
+        int defaultHotbarAttackIndicatorOffset = 23;
 
         int amount = 0;
         for (ItemStack itemStack : playerEntity.getArmorItems())
             if (!itemStack.isEmpty())
                 amount++;
 
-        if (amount > 0 || currentConfig.getWidgetShown() == ArmorHudConfig.WidgetShown.ALWAYS) {
+        if (amount > 0 || currentArmorHudConfig.getWidgetShown() == ArmorHudConfig.WidgetShown.ALWAYS) {
             int sideMultiplier;
             int sideOffsetMultiplier;
-            if ((currentConfig.getAnchor() == ArmorHudConfig.Anchor.HOTBAR && currentConfig.getSide() == ArmorHudConfig.Side.LEFT) || (currentConfig.getAnchor() != ArmorHudConfig.Anchor.HOTBAR && currentConfig.getSide() == ArmorHudConfig.Side.RIGHT)) {
+            if ((currentArmorHudConfig.getAnchor() == ArmorHudConfig.Anchor.HOTBAR && currentArmorHudConfig.getSide() == ArmorHudConfig.Side.LEFT) || (currentArmorHudConfig.getAnchor() != ArmorHudConfig.Anchor.HOTBAR && currentArmorHudConfig.getSide() == ArmorHudConfig.Side.RIGHT)) {
                 sideMultiplier = -1;
                 sideOffsetMultiplier = -1;
             } else {
@@ -72,7 +84,7 @@ public abstract class InGameHudMixin extends DrawableHelper {
             }
 
             int verticalMultiplier;
-            switch (currentConfig.getAnchor()) {
+            switch (currentArmorHudConfig.getAnchor()) {
                 case TOP:
                 case TOP_CENTER:
                     verticalMultiplier = 1;
@@ -82,40 +94,40 @@ public abstract class InGameHudMixin extends DrawableHelper {
                     verticalMultiplier = -1;
                     break;
                 default:
-                    throw new IllegalStateException("Unexpected value: " + currentConfig.getAnchor());
+                    throw new IllegalStateException("Unexpected value: " + currentArmorHudConfig.getAnchor());
             }
 
-            int offhandSlotOffset;
-            switch (currentConfig.getOffhandSlotBehavior()) {
+            int addedHotbarOffset;
+            switch (currentArmorHudConfig.getOffhandSlotBehavior()) {
                 case ALWAYS_IGNORE:
-                    offhandSlotOffset = 0;
+                    addedHotbarOffset = 0;
                     break;
                 case ALWAYS_LEAVE_SPACE:
-                    offhandSlotOffset = defaultOffhandSlotOffset;
+                    addedHotbarOffset = Math.max(defaultOffhandSlotOffset, defaultHotbarAttackIndicatorOffset);
                     break;
                 case ADHERE:
                     if (
                             (
-                                    (
-                                            (playerEntity.getMainArm().getOpposite() == Arm.LEFT && currentConfig.getSide() == ArmorHudConfig.Side.LEFT)
-                                                    || (playerEntity.getMainArm().getOpposite() == Arm.RIGHT && currentConfig.getSide() == ArmorHudConfig.Side.RIGHT)
-                                    ) && !playerEntity.getOffHandStack().isEmpty()
-                            ) || (
-                                    (
-                                            (playerEntity.getMainArm() == Arm.LEFT && currentConfig.getSide() == ArmorHudConfig.Side.LEFT)
-                                                    || (playerEntity.getMainArm() == Arm.RIGHT && currentConfig.getSide() == ArmorHudConfig.Side.RIGHT)
-                                    ) && this.client.options.attackIndicator == AttackIndicator.HOTBAR // todo make independent
-                            )
+                                    playerEntity.getMainArm().getOpposite() == Arm.LEFT && currentArmorHudConfig.getSide() == ArmorHudConfig.Side.LEFT
+                                            || playerEntity.getMainArm().getOpposite() == Arm.RIGHT && currentArmorHudConfig.getSide() == ArmorHudConfig.Side.RIGHT
+                            ) && !playerEntity.getOffHandStack().isEmpty()
                     )
-                        offhandSlotOffset = defaultOffhandSlotOffset;
+                        addedHotbarOffset = defaultOffhandSlotOffset;
+                    else if (
+                            (
+                                    playerEntity.getMainArm() == Arm.LEFT && currentArmorHudConfig.getSide() == ArmorHudConfig.Side.LEFT
+                                            || playerEntity.getMainArm() == Arm.RIGHT && currentArmorHudConfig.getSide() == ArmorHudConfig.Side.RIGHT
+                            ) && this.client.options.attackIndicator == AttackIndicator.HOTBAR
+                    )
+                        addedHotbarOffset = defaultHotbarAttackIndicatorOffset;
                     else
-                        offhandSlotOffset = 0;
+                        addedHotbarOffset = 0;
                     break;
                 default:
-                    throw new IllegalStateException("Unexpected value: " + currentConfig.getOffhandSlotBehavior());
+                    throw new IllegalStateException("Unexpected value: " + currentArmorHudConfig.getOffhandSlotBehavior());
             }
 
-            switch (currentConfig.getAnchor()) {
+            switch (currentArmorHudConfig.getAnchor()) {
                 case BOTTOM:
                 case HOTBAR:
                     this.armorWidgetY = this.scaledHeight - height;
@@ -125,13 +137,13 @@ public abstract class InGameHudMixin extends DrawableHelper {
                     this.armorWidgetY = 0;
                     break;
                 default:
-                    throw new IllegalStateException("Unexpected value: " + currentConfig.getAnchor());
+                    throw new IllegalStateException("Unexpected value: " + currentArmorHudConfig.getAnchor());
             }
 
-            int slots = currentConfig.getWidgetShown() == ArmorHudConfig.WidgetShown.NOT_EMPTY ? amount : 4;
+            int slots = currentArmorHudConfig.getWidgetShown() == ArmorHudConfig.WidgetShown.NOT_EMPTY ? amount : 4;
             int widgetWidth = width + ((slots - 1) * step);
 
-            switch (currentConfig.getAnchor()) {
+            switch (currentArmorHudConfig.getAnchor()) {
                 case TOP_CENTER:
                     this.armorWidgetX = center - (widgetWidth / 2);
                     break;
@@ -140,14 +152,14 @@ public abstract class InGameHudMixin extends DrawableHelper {
                     this.armorWidgetX = (widgetWidth - this.scaledWidth) * sideOffsetMultiplier;
                     break;
                 case HOTBAR:
-                    this.armorWidgetX = center + ((defaultHotbarOffset + offhandSlotOffset) * sideMultiplier) + (widgetWidth * sideOffsetMultiplier);
+                    this.armorWidgetX = center + ((defaultHotbarOffset + addedHotbarOffset) * sideMultiplier) + (widgetWidth * sideOffsetMultiplier);
                     break;
                 default:
-                    throw new IllegalStateException("Unexpected value: " + currentConfig.getAnchor());
+                    throw new IllegalStateException("Unexpected value: " + currentArmorHudConfig.getAnchor());
             }
 
-            this.armorWidgetX += currentConfig.getOffsetX() * sideMultiplier;
-            this.armorWidgetY += currentConfig.getOffsetY() * verticalMultiplier;
+            this.armorWidgetX += currentArmorHudConfig.getOffsetX() * sideMultiplier;
+            this.armorWidgetY += currentArmorHudConfig.getOffsetY() * verticalMultiplier;
 
             int endPieceLength = 3;
             this.drawTexture(matrices, this.armorWidgetX, this.armorWidgetY, 0, 0, widgetWidth - endPieceLength, height);
@@ -160,15 +172,47 @@ public abstract class InGameHudMixin extends DrawableHelper {
         PlayerEntity playerEntity = this.getCameraPlayer();
         ArrayList<ItemStack> armorItems = new ArrayList<>(4);
         for (ItemStack itemStack : playerEntity.getArmorItems())
-            if (!itemStack.isEmpty() || currentConfig.getWidgetShown() != ArmorHudConfig.WidgetShown.NOT_EMPTY)
+            if (!itemStack.isEmpty() || currentArmorHudConfig.getWidgetShown() != ArmorHudConfig.WidgetShown.NOT_EMPTY)
                 armorItems.add(itemStack);
 
         int step = 20;
+        int amount = 0;
         for (int i = 0; i < armorItems.size(); i++) {
-            int iReversed = currentConfig.isReversed() ? (armorItems.size() - i - 1) : i;
-            if (!armorItems.get(i).isEmpty())
+            int iReversed = currentArmorHudConfig.isReversed() ? (armorItems.size() - i - 1) : i;
+            if (!armorItems.get(i).isEmpty()) {
                 this.renderHotbarItem(this.armorWidgetX + (step * iReversed) + 3, this.armorWidgetY + 3, tickDelta, playerEntity, armorItems.get(i));
-            //todo add empty icon rendering (shift+f11)
+                amount++;
+            }
+        }
+
+        if (currentArmorHudConfig.getWidgetShown() != ArmorHudConfig.WidgetShown.NOT_EMPTY && (amount > 0 || currentArmorHudConfig.getWidgetShown() == ArmorHudConfig.WidgetShown.ALWAYS)) {
+            RenderSystem.blendFuncSeparate(GlStateManager.SrcFactor.ONE, GlStateManager.DstFactor.ONE_MINUS_SRC_COLOR, GlStateManager.SrcFactor.ONE, GlStateManager.DstFactor.ZERO);
+            for (int i = 0; i < armorItems.size(); i++) {
+                if (armorItems.get(i).isEmpty()) {
+                    Identifier spriteId;
+                    switch (i) {
+                        case 0:
+                            spriteId = EMPTY_BOOTS_SLOT_TEXTURE;
+                            break;
+                        case 1:
+                            spriteId = EMPTY_LEGGINGS_SLOT_TEXTURE;
+                            break;
+                        case 2:
+                            spriteId = EMPTY_CHESTPLATE_SLOT_TEXTURE;
+                            break;
+                        case 3:
+                        default:
+                            spriteId = EMPTY_HELMET_SLOT_TEXTURE;
+                            break;
+                    }
+                    Sprite sprite = this.client.getSpriteAtlas(BLOCK_ATLAS_TEXTURE).apply(spriteId);
+                    this.client.getTextureManager().bindTexture(sprite.getAtlas().getId());
+
+                    int iReversed = currentArmorHudConfig.isReversed() ? (armorItems.size() - i - 1) : i;
+                    drawSprite(matrices, this.armorWidgetX + (step * iReversed) + 3, this.armorWidgetY + 3, getZOffset() + 1, 16, 16, sprite);
+                }
+            }
+            RenderSystem.defaultBlendFunc();
         }
     }
 
