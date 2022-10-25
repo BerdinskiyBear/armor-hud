350f186f304e0ce2770ad5b814b402f72348e06f
Julienraptor01 <julienraptor01@gmail.com>
2022-10-25 19:46:54 +0200

    Update to 1.19.2
    * Small fix to French localization
    
    * Update dependencies
    
    * Update to new features (different Random, TranslatableText...)

--- src/main/java/ru/berdinskiybear/armorhud/mixin/InGameHudMixin.java
+++ src/main/java/ru/berdinskiybear/armorhud/mixin/InGameHudMixin.java
@@ -14,6 +14,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Arm;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
+import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -25,7 +26,6 @@ import ru.berdinskiybear.armorhud.ArmorHudMod;
 import ru.berdinskiybear.armorhud.config.ArmorHudConfig;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin extends DrawableHelper {
@@ -154,7 +154,7 @@ public abstract class InGameHudMixin extends DrawableHelper {
                                         (
                                                 playerEntity.getMainArm() == Arm.LEFT && currentArmorHudConfig.getSide() == ArmorHudConfig.Side.LEFT
                                                         || playerEntity.getMainArm() == Arm.RIGHT && currentArmorHudConfig.getSide() == ArmorHudConfig.Side.RIGHT
-                                        ) && this.client.options.attackIndicator == AttackIndicator.HOTBAR
+                                        ) && this.client.options.getAttackIndicator().getValue() == AttackIndicator.HOTBAR
                                 )
                                     addedHotbarOffset = armorHud_defaultHotbarAttackIndicatorOffset;
                                 else
